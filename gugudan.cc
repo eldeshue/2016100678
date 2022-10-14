0d5fc14eb7c8c99f01c1cd27193630adaa5f6b5e
Herrtane <herrtane@gmail.com>
2022-10-14 14:41:12 +0900

    modify gugudan.cc

--- gugudan.cc
+++ gugudan.cc
@@ -4,9 +4,9 @@
 
 int main(){
 	std::cout<< " printing gugudan " << std::endl;
-	for(int i = 1; i < 9; i++){ // modify here
+	for(int i = 1; i < 10; i++){ // modify here
 		std::cout<< " --- " << i << " 단" << " --- " <<std::endl;
-		for(int j = 1; j <= 9; j++){
+		for(int j = 1; j <= 10; j++){
 			std::cout<< " " << i << " X " << j << " = " << i*j <<std::endl;
 		}
 	}
